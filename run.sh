93aa21ba8b5d99a8c8fa51b44ae3897daebdc82c
enzokro <christopher.kroenke@gmail.com>
2024-03-28 14:01:29 -0400

    add endpoint to run.sh script

--- run.sh
+++ run.sh
@@ -6,20 +6,26 @@
 usage() {
     echo "Usage: $0 [option]"
     echo "Options:"
-    echo "  -a, --all         Run all commands (default)"
+    echo "  -l, --local       Run the local client/server (default)"
     echo "  -s, --server      Run the server command"
     echo "  -f, --find        Run the find command"
+    echo "  -e, --endpoint    Run the endpoint command"
     echo "  -h, --help        Display this help message"
 }
 
 run_server() {
-    echo "Running server command..."
+    echo "Running local server..."
     python see_mp/server.py &
 }
 
 run_find() {
-    echo "Running find command..."
+    echo "Running local client..."
     python see_mp/client.py &
+}
+
+run_endpoint() {
+    echo "Running the endpoint..."
+    python see_mp/endpoint.py &
 }
 
 # Function to handle the termination of processes
@@ -28,12 +34,13 @@ terminate_processes() {
     # Option 1: Use pkill for pattern matching, if applicable
     pkill -f 'python see_mp/server.py'
     pkill -f 'python see_mp/client.py'
+    pkill -f 'python see_mp/endpoint.py'
 
     # # Option 2: Explicitly kill process groups
     # kill -- -$server_pid
     # kill -- -$find_pid
 
-    wait $server_pid $find_pid 2>/dev/null
+    wait $server_pid $find_pid $endpoint_pid 2>/dev/null
     exit 0
 }
 
@@ -42,7 +49,7 @@ trap terminate_processes SIGINT SIGTERM
 
 # Parse command-line arguments
 case "$1" in
-    -a|--all)
+    -l|--local)
         run_server
         server_pid=$!
         run_find
@@ -58,6 +65,11 @@ case "$1" in
         run_find
         find_pid=$!
         wait $find_pid
+        ;;
+    -e|--endpoint)
+        run_endpoint
+        endpoint_pid=$!
+        wait $endpoint_pid
         ;;
     -h|--help)
         usage

544ce1424e9c5f9c5ef9c3e9b9d91120b79b2b99
enzokro <christopher.kroenke@gmail.com>
2024-03-28 13:32:28 -0400

    server cleanup to use utils and config

--- see_mp/server.py
+++ see_mp/server.py
@@ -1,18 +1,19 @@
 import os
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
+from see_mp.utils import Config
 
+
+# create the flask app
 app = Flask(__name__)
 socketio = SocketIO(app, logger=True, engineio_logger=True)
 
-# Configuration
-HOST = os.environ.get('HOST', 'localhost')
-PORT = int(os.environ.get('PORT', 8989))
 
 @app.route('/')
 def index():
     """Renders the main site."""
     return render_template('index.html')
+
 
 @app.route('/detections', methods=['POST'])
 def handle_detection():
@@ -20,14 +21,14 @@ def handle_detection():
     detection_data = request.json
     print(f"Received detection data: {detection_data}")
     
-    # Emit the detection result to connected WebSocket clients
+    # Emit the detection result 
     socketio.emit('detection_result', detection_data)
     
     return jsonify({"status": "received"})
 
 def run_server():
     """Runs the application."""
-    socketio.run(app, host=HOST, port=PORT)
+    socketio.run(app, host=Config.HOST, port=Config.PORT)
 
 if __name__ == '__main__':
     try:
